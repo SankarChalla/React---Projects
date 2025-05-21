725cf2e84e82f06858fb3ed74fafe1540f68eb6c
sankar <cvbsankar@ivp.in>
2025-05-21 16:06:00 +0530

    1st Navigation Section

--- advanced-filtering/src/Navigation/Nav.jsx
+++ advanced-filtering/src/Navigation/Nav.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import '../Navigation/Nav.css'
+import { FiHeart } from 'react-icons/fi'
+import { AiOutlineShoppingCart,AiOutlineUser } from "react-icons/ai"
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <div className="nav-container">
+        <input 
+        type="text"
+        className='search-input'
+        placeholder='Enter your search shoes.' 
+        />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className='nav-icons' />
+        </a>
+        <a href="#">
+          <AiOutlineUser className='nav-icons' />
+        </a>
+      </div>
+    </nav>
   )
 }
 
