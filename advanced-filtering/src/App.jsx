cec446c13d20e5214c7c98b6620c43452078aa67
sankar <cvbsankar@ivp.in>
2025-05-21 20:38:21 +0530

    basic Sidebar component created

--- advanced-filtering/src/App.jsx
+++ advanced-filtering/src/App.jsx
@@ -1,3 +1,4 @@
+import Sidebar from "./Sidebar/Sidebar"
 import Nav from "./Navigation/Nav"
 import Products from "./Products/Products"
 import Recommended from './Recommended/Recommended'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+      <Sidebar />
       <Nav />
       <Recommended />
       <Products />
---
+++ advanced-filtering/src/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import Category from './Category/Category'
+import Price from './Price/Price'
+import Colors from './Colors/Colors'
+import '../Sidebar/Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <section className="sidebar">
+      <div className="logo-container">
+        <h1>🛒</h1>
+      </div>
+      <Category />
+      <Colors />
+      <Price />
+    </section>
+  )
+}
+
+export default Sidebar
