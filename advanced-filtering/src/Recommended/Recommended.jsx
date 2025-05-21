03f3f0512bc264fba7a745b93fb1a09866acaa7e
sankar <cvbsankar@ivp.in>
2025-05-21 15:17:46 +0530

    Intitial Commit

---
+++ advanced-filtering/src/Recommended/Recommended.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Recommended/Recommended.css'
+
+const Recommended = () => {
+  return (
+    <div>Recommended</div>
+  )
+}
+
+export default Recommended
---
+++ advanced-filtering/src/Sidebar/Category/Category.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Category/Category.css'
+
+const Category = () => {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category
