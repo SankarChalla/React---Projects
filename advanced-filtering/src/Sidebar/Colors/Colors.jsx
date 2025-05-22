e2829265b55e9598867db0a83663d63705a4974a
sankar <cvbsankar@ivp.in>
2025-05-22 14:54:44 +0530

    color component created

--- advanced-filtering/src/Sidebar/Colors/Colors.jsx
+++ advanced-filtering/src/Sidebar/Colors/Colors.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "../Colors/Colors.css";
 
 const Colors = () => {
   return (
-    <div>Colors</div>
-  )
-}
+    <div>
+      <h2 className="sidebar-title color-title">Color</h2>
+      <label className="sidebar-label-container color-title">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>All
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>Black
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>Blue
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>Red
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>Green
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"></span>White
+      </label>
+    </div>
+  );
+};
 
-export default Colors
+export default Colors;
